7286493bca5f37b496c9b13ae6bcfeb1915c646c
GeorgiyNaumenko <58660010+GeorgiyNaumenko@users.noreply.github.com>
2020-11-04 15:12:27 +0200

    Add files via upload

--- Flowers_Boquet/src/Flowers/Boquet.java
+++ Flowers_Boquet/src/Flowers/Boquet.java
@@ -4,14 +4,25 @@ import java.util.ArrayList;
 public class Boquet {
 
 	private ArrayList <Flower> flowers = new ArrayList<>();
-	private int length = 0;
+	
+	Boquet(){
+		
+	}
+	
+	Boquet(ArrayList <Flower> _flowers){
+		flowers = _flowers;
+	}
+	
+	protected int getLength() {
+		return flowers.size();
+	}
 	
 	protected void addFlower(Flower newFlower) {
-		if (length == 0) {
+		if (getLength() == 0) {
 			flowers.add(newFlower);
 		}
 		else {
-			int left = 0, right = length;
+			int left = 0, right = getLength();
 			while(left < right) {
 				int m = left + (right - left) / 2;
 				if (newFlower.length() >= flowers.get(m).length()) {
@@ -29,5 +40,10 @@ public class Boquet {
 		return flowers.remove(flower);
 	}
 	
+	protected ArrayList <Flower> getForRange(float a, float b){
+		ArrayList <Flower> res = new ArrayList<>();
+		
+		return res;
+	}
 	
 }
--- Flowers_Boquet/src/Flowers/Flower.java
+++ Flowers_Boquet/src/Flowers/Flower.java
@@ -6,7 +6,52 @@ public class Flower {
 	private int leaves;
 	private double length;
 	
+	Flower() {
+		
+	}
+	
+	Flower(String _color, String _name, int _leaves, double _length) {
+		color = _color;
+		name = _name;
+		leaves = _leaves;
+		length = _length;
+	}
+	
+	@Override
+	public String toString() {
+		return name + ", " + color + ", довжина: " + length + ", кількість листків: " + leaves;
+	}
+	
 	protected double length() {
 		return length;
 	}
+	
+	protected String getName() {
+		return name;
+	}
+	
+	protected String getColor() {
+		return color;
+	}
+	
+	protected int getLeaves() {
+		return leaves;
+	}
+	
+	protected void setLength(double newLength) {
+		length = newLength;
+	}
+	
+	protected void setName(String newName) {
+		name = newName;
+	}
+	
+	protected void setColor(String newColor) {
+		color = newColor;
+	}
+	
+	protected void setLeaves(int newLeaves) {
+		leaves = newLeaves;
+	}
+	
 }
